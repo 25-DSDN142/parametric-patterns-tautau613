f89b39ff1f104f4e47f1a3665de51faf626a169f
tautau613 <lovehineatauira@gmail.com>
2025-07-31 13:31:32 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,18 +1,18 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
+let rect_width  = 50;
+let rect_height = 50;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_height = 200
   pWallpaper.grid_settings.row_offset  = 50;
 }
 
